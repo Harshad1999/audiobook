87f7509948cac81b2353fecc70e8c2ae3c406dd4
Harshad1999 <sonawaneharshad1999@gmail.com>
2020-10-08 23:57:54 +0530

    tp

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pyttsx3
 import PyPDF2
-book = open('the-rudest-book-ever-shwetabh-gangwar.pdf' , 'rb')
+book = open('the-rudest-book-ever-shwetabh-gangwar.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-speaker.say('Hey Harshad I am All yours')
+page = pdfReader.getPage(7)
+text = page.extractText()
+speaker.say(text)
 speaker.runAndWait()
