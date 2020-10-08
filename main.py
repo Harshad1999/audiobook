943965045dd2838a52ab3da431dacbf7b171abde
Harshad1999 <sonawaneharshad1999@gmail.com>
2020-10-09 00:01:16 +0530

    for loop added

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-page = pdfReader.getPage(7)
-text = page.extractText()
-speaker.say(text)
-speaker.runAndWait()
+for num in range(7, pages):
+    page = pdfReader.getPage(7)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
